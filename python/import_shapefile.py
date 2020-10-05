649bf23322b992f6d9d0659e8edc23370b8aa4a4
Philip Bailey <philip@northarrowresearch.com>
2020-10-05 15:01:04 -0700

    import script working for Idaho

--- python/import_shapefile.py
+++ python/import_shapefile.py
@@ -1,10 +1,14 @@
 import os
+import json
 import argparse
 import psycopg2
+import getpass
+import datetime
 from psycopg2.extras import execute_values
 from osgeo import ogr
 from rscommons import Logger, ProgressBar, dotenv
 from rscommons.util import batch
+from rscommons.shapefile import get_transform_from_epsg
 
 
 def import_shapefile(shapefile, host, port, user_name, password, database):
@@ -19,19 +23,68 @@ def import_shapefile(shapefile, host, port, user_name, password, database):
     data_source = driver.Open(shapefile)
     layer = data_source.GetLayer()
     total_features = layer.GetFeatureCount()
+    _spatial_ref, transform = get_transform_from_epsg( layer.GetSpatialRef(), 4326)
+
+    curs.execute('INSERT INTO uploads (added_by, file_name, remarks) VALUES (%s, %s, %s) RETURNING id', 
+        [getpass.getuser(), os.path.basename(shapefile), 'Python Script Import'])
+    upload_id = curs.fetchone()[0]
+
+    curs.execute('SELECT name, id FROM observation_types')
+    obs_types = {row[0].replace('Dam', '').replace(' ', '').lower(): row[1] for row in curs.fetchall()}
+
+    certainties = {'Low': 1, 'Medium': 2, 'High': 3}
 
     # Reach statistics for each reach in our batch
     progbar = ProgressBar(total_features, 50, "Importing features")
 
     try:
+        observations = []
         for feature in layer:
+            geom = feature.GetGeometryRef()
+            geom.Transform(transform)
+            geom.FlattenTo2D()
+
+            metadata = {field: feature.GetField(field) for field in [
+                'Feature_Ty', 'Certainty', 'Year', 'Dam_Type', 'CreationDa', 
+                'Creator', 'EditDate', 'Editor', 'Snapped', 'Imagery_Ye'] }
+
+            # print(metadata)
+
+            clean_type = feature.GetField('Feature_Ty').replace('_', '').replace('Dam', '').lower() if feature.GetField('Feature_Ty') else 'Unknown'.lower()
+            obs_date = datetime.datetime.strptime(feature.GetField('CreationDa'), '%Y/%m/%d')
+            year = int(feature.GetField('Year')) if feature.GetField('Year') else None
+            certainty = certainties[feature.GetField('Certainty')] if feature.GetField('Certainty') in certainties else 0
+
+            observations.append((
+                upload_id,
+                geom.ExportToWkb(),
+                obs_date,
+                feature.GetField('Creator'),
+                year,
+                obs_types[clean_type],
+                True,
+                certainty,
+                json.dumps(metadata)
+            ))
             
-            geom_processed += 1
-            progbar.update(geom_processed)
+            progbar.update(len(observations))
 
+        progbar.finish()
 
-
+        curs.executemany("""
+            INSERT INTO observations (
+                upload_id,
+                geom,
+                obs_date,
+                observer,
+                obs_year,
+                obs_type_id,
+                is_public,
+                confidence,
+                metadata
+            ) VALUES (%s, ST_GeomFromWKB(%s, 4326), %s, %s, %s, %s, %s, %s, %s)""", observations)
         conn.commit()
+        
     except Exception as ex:
         conn.rollback()
         log.error(ex)
